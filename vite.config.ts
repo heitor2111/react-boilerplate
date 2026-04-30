3932b5c6b35b95de7421eaaeab9d3bbd659cc0a8
Heitor Henrique <heitor2111@gmail.com>
2026-04-30 12:19:44 -0300

    aliases para a pasta src configurados

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 
+import path from "path";
+import pkg from "./package.json";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,4 +13,12 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
