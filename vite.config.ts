e0722bb5965b4d28263c9f54391840fbaa6d68ba
Heitor Henrique <heitor2111@gmail.com>
2026-04-30 16:10:57 -0300

    iconify e unplugin-icons instalados e configurados

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import path from 'path'
+import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
 
 import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+    Icons({ compiler: 'jsx', jsx: 'react' }),
+  ],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
