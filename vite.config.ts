3800687c475d3f9c91306692ec1e60a037c27528
Heitor Henrique <heitor2111@gmail.com>
2026-04-30 12:23:58 -0300

    executa o prettier para formatação dos arquivos padrão

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
-import tailwindcss from "@tailwindcss/vite";
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
-import path from "path";
-import pkg from "./package.json";
+import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-});
+})
